c213c537ae0e36667a71717f9c78db783a36bdec
Connor Meth <connormeth27@gmail.com>
2020-01-08 17:51:02 -0500

    almost done. need to fill in resume info

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -53,24 +53,21 @@ class Resume extends Component {
 
 
 
-      <div className="row skill">
+    <div className="row skill">
 
-         <div className="three columns header-col">
-            <h1><span>Skills</span></h1>
-         </div>
-
-         <div className="nine columns main-col">
-
-            <p>{skillmessage}
-            </p>
-
-				<div className="bars">
-				   <ul className="skills">
-					  {skills}
-					</ul>
-				</div>
-			</div>
-      </div>
+        <div className="three columns header-col">
+          <h1><span>Skills</span></h1>
+        </div>
+        <div className="nine columns main-col">
+          <p>{skillmessage}
+          </p>
+				  <div className="bars">
+				    <ul className="skills">
+				    {skills}
+				    </ul>
+				  </div>
+			  </div>
+    </div>
    </section>
     );
   }
